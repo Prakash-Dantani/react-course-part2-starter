07f3089377ad4a401417dd134726ff26a967e3e6
Prakash Dantani <dantaniprakash08@gmail.com>
2023-12-16 07:46:20 +0530

    Customize hooks and parameterize query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <TodoList />;
+  return (
+    <>
+      <h1>Post List</h1>
+      <PostList />
+      {/* <h1>Todo List</h1>
+      <TodoList /> */}
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,28 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300000, // 5 Min
+      staleTime: 10 * 1000, // after 10 Second refresh data
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );

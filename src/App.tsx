336c4303fd056f0a280aed02b60f7f11e25c672d
Prakash Dantani <dantaniprakash08@gmail.com>
2023-12-16 08:06:30 +0530

    PAgination Queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import Pagination from "./react-query/Pagination";
 import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <>
-      <h1>Post List</h1>
-      <PostList />
+      <h1>Pagination List</h1>
+      <Pagination />
+      <br />
+      {/* <h1>Post List</h1>
+      <PostList /> */}
       {/* <h1>Todo List</h1>
       <TodoList /> */}
     </>
---
+++ src/react-query/Pagination.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import usePosts from "../hooks/usePosts";
+
+const Pagination = () => {
+  const pageSize = 10;
+  const [page, setPage] = useState<number>(1);
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+
+  if (isLoading) return <p>Loading ...</p>;
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <button
+        className="btn btn-primary my-3"
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+      >
+        Previous
+      </button>
+
+      <button
+        disabled={posts.length < 1}
+        className="btn btn-primary my-3 ms-1"
+        onClick={() => setPage(page + 1)}
+      >
+        Next
+      </button>
+    </>
+  );
+};
+
+export default Pagination;

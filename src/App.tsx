6052c9282b50d0855796fbd72cd5d8e8c64ac911
Prakash Dantani <dantaniprakash08@gmail.com>
2023-12-17 08:40:22 +0530

    Mutating Data and HAndle Mutating error

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import "./App.css";
+import InfinitList from "./react-query/InfinitList";
 import Pagination from "./react-query/Pagination";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <>
-      <h1>Pagination List</h1>
-      <Pagination />
-      <br />
       {/* <h1>Post List</h1>
       <PostList /> */}
-      {/* <h1>Todo List</h1>
-      <TodoList /> */}
+      <TodoForm />
+      <br />
+      <h1>Todo List</h1>
+      <TodoList />
+      <br />
+
+      {/* <h1>Pagination List</h1>
+      <Pagination />
+      <br />
+      <h1>Infinity List</h1>
+      <InfinitList /> */}
+      <br />
     </>
   );
 }

be9a13704d449d429a6442a34b5163f3d3dfc4c7
Prakash Dantani <dantaniprakash08@gmail.com>
2023-12-17 12:50:35 +0530

    Creating Reusable API Clients

--- src/hooks/useAddTodo.ts
+++ src/hooks/useAddTodo.ts
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-// import { useRef } from "react";
+
 import { Todo } from "./useTodos";
 import { CACHE_KEY_TODOS } from "../react-query/constants";
+import APIClient from "../react-query/services/apiClients";
 
+const apiClient = new APIClient<Todo>('/todos');
 interface AddTodoContext {
     previousTodo?: Todo[];
   }
@@ -13,11 +15,8 @@ const useAddTodo = (onAdd:()=>void) => {
     const queryClient = useQueryClient();
 
   return useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-        .then((res) => res.data),
-
+    mutationFn: apiClient.post,  
+    
     onMutate: (newTodo: Todo) => {
       // Get Todos List before saving datainto chache because if serve face some error than we have to make list in its actual list.
       const previousTodo = queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS);
--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,24 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import APIClient from "../react-query/services/apiClients";
 import { CACHE_KEY_TODOS } from "../react-query/constants";
 
+const apiClient = new APIClient<Todo>('/todos');
 export interface Todo {
     id: number;
     title: string;
     userId: number;
     completed: boolean;
   }
-  
 
 const useTodos = () =>{
-    const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-
+    
       return useQuery<Todo[], Error>({
         queryKey: CACHE_KEY_TODOS,
-        queryFn: fetchTodos,
+        queryFn: apiClient.getAll,
         staleTime:10*1000,
       })
 }
